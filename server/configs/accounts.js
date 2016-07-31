903b59ed9494474ae30bce8e1ff451181f512b7f
Reno Meyer <reno.meyer@outlook.com>
2016-07-31 17:54:59 +0200

    email notification

--- server/configs/accounts.js
+++ server/configs/accounts.js
@@ -15,15 +15,18 @@ export default () => {
         to: email,
         from: Meteor.settings.private.admin_email,
         subject: `Invitation ${Meteor.settings.public.app_name} app`,
-        html: `<p>Hi ${user.profile.firstname},
-          you are receiving this mail because you have been invited to join the ${Meteor.settings.public.app_name} app.
-          Logging in is quite easy. Simply use this url:
+        html: `<h3>Hoi ${user.profile.firstname}</h3>
+          <p>Du bist herzlich eingeladen die neue, super coole ${Meteor.settings.public.app_name} App zu nutzen! 
+          Darauf findest du alle Informationen bezüglich Programm alljährlichen Treffens der ${Meteor.settings.public.app_name}-Elite 
+          und kannst dich für alle Events anmelden, an denen du Teilnehmen möchtest.</p>
+          <p>Die ${Meteor.settings.public.app_name} App dient auch gleich als Dropbox-Schnittstelle, wo alle ganz einfach ihre Bilder
+          hochladen können.</p>
+          <p>Mit dem folgenden link logst du dich automatisch ein:</p>
 
           <a href="${Meteor.settings.public.app_url}/login/${email}/${options.initpass}">${Meteor.settings.public.app_url}/login/${email}/${options.initpass}</a>
-
-          Make sure you don't share this url with anybody!
-          the ${Meteor.settings.public.app_name} team.
-        </p>`,
+          
+          <p>Also, check that shit out und falls noch fragen sind, einfach den Host fragen...
+          </p>`,
       });
     }
 
