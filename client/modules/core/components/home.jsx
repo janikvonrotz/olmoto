8e871091ffa456648963afa3c5f9d68b5a2f54a7
Reno Meyer <reno.meyer@outlook.com>
2016-07-31 13:41:24 +0200

    app bar title

--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import Page from '../containers/page';
 
 class Home extends React.Component {
@@ -7,7 +8,7 @@ class Home extends React.Component {
   }
 
   componentDidMount(){
-    this.props.setLocalState({title: "Home"})
+    this.props.setLocalState({title: Meteor.settings.public.app_name})
   }
 
   render() {
